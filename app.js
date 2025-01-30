7455abec79a1b6d444a4dc19525f8f2670a533a4
MOH2056 <tapereyetunde@gmail.com>
2025-01-30 15:57:29 +0100

    feat: add API entry point with Express

---
+++ app.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import router from './routes/info.js'
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("json spaces", 4);
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', router)
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on ${PORT}`)
+})
