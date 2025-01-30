1e55cc29476ec66afaf7ce12453b1c77e1ee136d
MOH2056 <tapereyetunde@gmail.com>
2025-01-31 00:29:44 +0100

    Add test GET route in entry file

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.json('Workinng fine')
+})
 app.set("json spaces", 4);
 
 app.use(cors())
