3401cdf0efa85fd16835da94c918ef364b9f4927
MOH2056 <tapereyetunde@gmail.com>
2025-01-31 02:03:06 +0100

    testing

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import router from './routes/info.js'
+//import router from './routes/info.js'
 
 dotenv.config()
 
+
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+app.get('/api/info', (req, res) => {
+    res.json({
+        "email": "tapereyetunde@gmail.com",
+        "current_datetime": new Date().toISOString(),
+        "github_url": "https://github.com/MOH2056/Public_API.git"
+    })
+})
+
 
 app.set("json spaces", 4);
 
